a20458b1550a922844cf9c3f07fcc905cf8804c5
廖澎 <631968524@qq.com>
2020-08-31 20:41:49 +0800

    git commit

---
+++ src/main/java/mybatis/entity/Apple.java
@@ -0,0 +1,11 @@
+package mybatis.entity;
+
+/**
+ * @author lp
+ * @date 2020/8/31 20:38
+ */
+public class Apple {
+    public static void main(String[] args) {
+        System.out.println("your are the apple of my eyes");
+    }
+}
